7402f3266e3d74ad108b352bedb1168c837f4466
pvspike <75666617+pvspike@users.noreply.github.com>
2021-03-15 15:40:30 +0200

    Delete includes directory

--- includes/DateTime.php
+++
@@ -1,4 +0,0 @@
-<?php
-$CurrentTime = time();
-$DateTime = strftime("%B-%d-%Y %H:%M:%S", $CurrentTime);
-echo $DateTime;
--- includes/google.php
+++
@@ -1,6 +0,0 @@
-<?php
-//reCAPTCHA Configuration - go to Google and get the below keys http://www.google.com/recaptcha/admin
-define('SITE_KEY',"xxx"); 
-define('SECRET_KEY',"xxx");
-define('SANTA_EMAIL',"santa@domain.com")
-?>
